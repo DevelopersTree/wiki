03671ed082e02faf676c5de3cdb560d4fdbec8d3
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-08-22 12:54:21 +0300

    configure netlify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
-  
-  module.exports = withBundleAnalyzer()
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = {
+  // Target must be serverless
+  target: "serverless",
+};
